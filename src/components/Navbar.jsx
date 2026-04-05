ef883ad52971103b15946d7e720e69e175ae38ce
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-04-05 17:46:33 +0500

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -271,7 +271,7 @@ export default function Navbar({ activePage, onNavigate, onLoginOpen }) {
               }}
             >
               <User size={16} />
-              Login / Sign Up
+              Login / Sign Up              //both login and signup will be handled in the same modal for simplicity
             </button>
           </div>
         </div>
