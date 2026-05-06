336da7f59ff08716c61f432306c46995b46ef5a6
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-05-06 21:57:25 +0500

    fix fles

--- client/src/Pages/Kids.jsx
+++ client/src/Pages/Kids.jsx
@@ -4,7 +4,7 @@ const Page = () => {
   return (
     <div className="pt-32 px-10 min-h-screen bg-white flex flex-col items-center justify-center text-center">
       <h1 className="font-serif text-5xl font-black uppercase tracking-tighter text-black">
-        Coming Soon
+        Coming Soon kids collection
       </h1>
       <p className="text-gray-400 mt-4 italic font-medium">
         We are currently curating the best collection for you.
