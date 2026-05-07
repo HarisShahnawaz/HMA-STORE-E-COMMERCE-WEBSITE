1b9f99012bf87b59ec965f39a92b3baf54be7676
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-05-07 16:49:55 +0500

    fix fles

--- client/src/Pages/Cart.jsx
+++ client/src/Pages/Cart.jsx
@@ -4,10 +4,10 @@ const Cart = () => {
   return (
     <div className="pt-32 px-10 min-h-screen bg-white">
       <h1 className="font-serif text-5xl font-black uppercase">Shopping Bag</h1>
-      <p className="text-gray-400 mt-4 italic text-lg">Your cart is currently empty.</p>
+      <p className="text-gray-400 mt-4 italic text-lg">Your cart is currently empty now .</p>
     </div>
   );
 };
 
-// THIS IS THE MISSING LINE:
+// THIS IS THE MISSING LINE of this:
 export default Cart;
